4b764be1b5d11fcb7d8aae84112812201d5590bf
Riyanwar Setiadi <riyanwarsetiadi0605@gmail.com>
2022-07-07 17:52:18 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex">
+    <Chat/>
+  </div>
+</template>
+
+<script>
+import Chat from "../components/chat.vue";
+export default {
+    name: "IndexPage",
+    data() {
+        return { arr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] };
+    },
+    components: { Chat }
+};
+</script>
